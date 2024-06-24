25b0bf1b30a5064b264969911068948d521fb9dd
Samuel Ratnam <samueljratnam@gmail.com>
2024-06-24 18:04:56 +0100

    underline effect nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <nav class="navbar flex justify-center bg-gray-200 p-8 text-xl">
     <ul class="nav-links flex list-none m-0 p-0">
-        <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700">home</a></li>
-        <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700">projects</a></li>
-        <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700">blog</a></li>
-        <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700">contact</a></li>
+        <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700 hover:underline">home</a></li>
+        <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700 hover:underline">projects</a></li>
+        <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700 hover:underline">blog</a></li>
+        <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700 hover:underline">contact</a></li>
     </ul>
 </nav>
