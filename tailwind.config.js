da93b87461cc74d21afc14754b3ac4745dd1e27c
Samuel Ratnam <samueljratnam@gmail.com>
2024-06-24 17:50:10 +0100

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    colors: {
+      black: '#000',
+      grey: '#DBDBDB',
+      blue: "0071A5"
+    },
+    extend: {}
+  },
+  plugins: []
+};
