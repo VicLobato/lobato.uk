cf0d9891cd1f66631a042646cb3b07f699787cce
Victor <vlobatogarcia@gmail.com>
2024-07-04 20:07:40 +0100

    minor changes or something

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <nav class="navbar flex justify-center bg-gray-200 p-8 text-xl">
     <ul class="nav-links flex list-none m-0 p-0">
         <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700 hover:underline">home</a></li>
-        <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700 hover:underline">projects</a></li>
+        <li class="mr-4"><a href="/projects" class="text-blue-800 hover:text-blue-700 hover:underline">projects</a></li>
         <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700 hover:underline">blog</a></li>
         <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700 hover:underline">contact</a></li>
     </ul>
