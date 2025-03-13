6adc38108b3f8fb8eaff5d34d3f4d7f70b618699
Victor <vlobatogarcia@gmail.com>
2025-03-13 22:24:03 +0000

    Something

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
-<nav class="navbar flex justify-center bg-gray-200 p-8 text-xl">
+<nav id="navbar" class="navbar flex justify-center bg-gray-200 p-8 text-xl">
     <ul class="nav-links flex list-none m-0 p-0">
-        <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700 hover:underline">home</a></li>
+        <li class="mr-4"><a href="" class="text-blue-800 hover:text-blue-700 hover:underline">home</a></li>
         <li class="mr-4"><a href="/projects" class="text-blue-800 hover:text-blue-700 hover:underline">projects</a></li>
-        <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700 hover:underline">blog</a></li>
-        <li class="mr-4"><a href="#" class="text-blue-800 hover:text-blue-700 hover:underline">contact</a></li>
+        <li class="mr-4"><a href="/resume" class="text-blue-800 hover:text-blue-700 hover:underline">resume</a></li>
+        <li class="mr-4"><a href="/contact" class="text-blue-800 hover:text-blue-700 hover:underline">contact</a></li>
     </ul>
 </nav>
 
